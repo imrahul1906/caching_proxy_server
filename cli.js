22239622d2f8132db33e9f8a2cd1055dd17ffe9c
Rahul Yadav <rahulyadav19.ry@gmail.com>
2025-05-31 16:41:49 +0530

    add figlet art

--- cli.js
+++ cli.js
@@ -1,12 +1,39 @@
 import { Command } from "commander";
+import figlet from "figlet";
 import { Controller } from "./controllers/Controller.js";
 
 const program = new Command();
 
+const banner = figlet.textSync('Caching Proxy Server');
+
+
 program
-    .name('Caching Proxy Server')
-    .description('A CLI tool that starts a caching proxy server')
-    .version('1.0');
+    .name("Caching Proxy Server")
+    .description("A CLI tool that starts a caching proxy server")
+    .version("1.0")
+    .configureHelp({
+        formatHelp: (cmd, helper) => {
+            const optionLines = cmd.options.map((option) => {
+                const term = helper.optionTerm(option);
+                const description = helper.optionDescription(option);
+                return `  ${term.padEnd(20)} ${description}`;
+            });
+
+            return [
+                figlet.textSync("Caching Proxy Server"),
+                "",
+                "Start the caching proxy server",
+                "",
+                "Commands:",
+                "  node cli.js load --url <target> [--port <port>]",
+                "",
+                "Options:",
+                ...optionLines,
+                "",
+                "Use '--help' with a command for more info",
+            ].join("\n");
+        },
+    })
 
 program
     .command('load')
